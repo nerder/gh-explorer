3f6aafa6770e24c80281f3de965253f1899c98ae
Stefano Saitta <stefano@buildo.io>
2016-01-21 20:18:17 +0100

    closes #3: create the app routes

---
+++ src/components/List.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { props, skinnable, t } from 'revenge';
+import { Link } from 'react-router-transition-context';
+
+@skinnable()
+@props({
+  list: t.Array
+})
+
+export default class List extends React.Component {
+
+  getLocals() {
+    const list = this.props.list;
+    return {
+      list
+    };
+  }
+
+  template({ list }) {
+    return (
+      <div>
+      {
+        list.map( (el) => {
+          return <div key={el}><Link key={el} to="details" params={{detailId: el}}>{el}</Link></div>;
+        })
+      }
+      </div>
+    );
+  }
+}
