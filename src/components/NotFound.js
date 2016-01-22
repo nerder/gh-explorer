4b5a94198f8c6c9ad5b17edc5bf608584d5ddfce
Stefano Saitta <stefano@buildo.io>
2016-01-22 11:24:10 +0100

    closes #6: insert not found Page

---
+++ src/components/NotFound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { skinnable } from 'revenge';
+
+@skinnable()
+
+export default class NotFound extends React.Component {
+
+  getLocals() {
+    return;
+  }
+
+  template() {
+    return (
+      <div>404 NOT FOUND</div>
+    );
+  }
+}
